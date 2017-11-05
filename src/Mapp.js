72d6592b52ff5afb2414bca29045fa7dd2f14640
floune <robin.sangla@live.fr>
2017-11-05 21:59:56 +0100

    <3

---
+++ src/Mapp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Mapp extends Component {
+
+  render() {
+    return (
+      <div className="Mapp">
+        <h1>Bataille Navale oé</h1>
+      </div>
+    );
+  }
+}
+
+export default Mapp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Mapp from './Mapp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Mapp />, document.getElementById('root'));
