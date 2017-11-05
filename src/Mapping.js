4aefd57561b5e5b3dc405a0a0342fb242dfa9f49
floune <robin.sangla@live.fr>
2017-11-05 23:07:20 +0100

    objet map

---
+++ src/Mapping.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Grille from './Grille';
+
+class Mapp extends Component {
+
+
+  render() {
+    return (
+        <Grille />
+      );
+  }
+}
+
+export default Mapp
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Mapp from './Mapp';
+import Mapp from './Mapping';
 
-ReactDOM.render(<Mapp />, document.getElementById('root'));
+ReactDOM.render(<Mapp />, document.getElementById('mapp'));
